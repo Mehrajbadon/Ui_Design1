d71074c544342e1ca627fd5d94a562aba202bd15
Mehraj Ibne Halim <87578499+Mehrajbadon@users.noreply.github.com>
2023-08-11 23:56:54 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
+
     );
   }
 }
@@ -32,102 +33,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<ItemModel> items = [];
 
-  @override
-  void initState() {
-    super.initState();
-    items.add(ItemModel(Icons.person, "Mahtab Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
-    items.add(ItemModel(Icons.person, "Aftab Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
-    items.add(ItemModel(Icons.person, "Mehraj Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
-    items.add(ItemModel(Icons.person, "Mehraj Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
-  }
+  int myindex = 0;
+  final tabs = [
+    Second(),
+  Third(),
+    Fourth()
+  ];
 
-  var color = Color(901813139);
+  var color1 = Color(904077344);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(leading: Icon(Icons.person),title: Center(child: Text("Moves")),actions: [Padding(
+        appBar: AppBar(leading: Icon(Icons.person,color: Colors.blueGrey,),
+          title: Center(child: Text("Moves",style: TextStyle(color: Colors.blueGrey,fontWeight: FontWeight.bold),)),actions: [Padding(
           padding: const EdgeInsets.only(right: 15),
-          child: Icon(Icons.notifications),
-        )],backgroundColor: color,),
-        body: Container(
-
-          color: color,
-          child: SafeArea(
-            child: Column(
-              children: [
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
-                          child: mehraj(
-                            items[index].icon,
-                            items[index].title,
-                            items[index].subtitle,
-                            items[index].imageAsset,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget mehraj(IconData icon, String title, String subtitle, String imageAsset) {
-    return Center(
-      child: Container(
-        // ... Container properties ...
-
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Card(
-                  // ... Card properties ...
-
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10, right: 8),
-                          child: ListTile(
-                            leading: CircleAvatar(child: Icon(icon)),
-                            title: Text(title,style: TextStyle(fontWeight: FontWeight.bold),),
-                            subtitle: Text(subtitle),
-                            trailing: Icon(Icons.more_horiz),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 20),
-                          child: Container(
-                            child: Image.asset(imageAsset,height: 150,width: 450,),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+          child: Icon(Icons.notifications,color: Colors.blueGrey,),
+        )],backgroundColor: Colors.white,),
+        body: tabs[myindex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              myindex = index;
+            });
+          },
+          currentIndex: myindex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Me"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+          ],
         ),
       ),
     );
