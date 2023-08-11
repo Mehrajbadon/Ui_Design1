d28966bb37afb77ef42e68465904889122883bd6
Mehraj Ibne Halim <87578499+Mehrajbadon@users.noreply.github.com>
2023-08-11 23:54:06 +0600

    Update item_model_class.dart

--- lib/item_model_class.dart
+++ lib/item_model_class.dart
@@ -1,5 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ItemModel {
   IconData icon;
@@ -9,4 +9,411 @@ class ItemModel {
 
   ItemModel(this.icon, this.title, this.subtitle, this.imageAsset);
 }
+
+class Second extends StatefulWidget {
+  const Second({super.key});
+
+  @override
+  State<Second> createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  List<ItemModel> items = [];
+  var color = Color(901813139);
+
+
+  @override
+  void initState() {
+    super.initState();
+    items.add(ItemModel(Icons.person, "Mahtab Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
+    items.add(ItemModel(Icons.person, "Aftab Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
+    items.add(ItemModel(Icons.person, "Mehraj Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
+    items.add(ItemModel(Icons.person, "Mehraj Ibne Halim", "Kabirhat, Noakhali", "asset/music3.jpg"));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+
+          child: Column(
+            children: [
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                        child: mehraj(
+                          items[index].icon,
+                          items[index].title,
+                          items[index].subtitle,
+                          items[index].imageAsset,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget mehraj(IconData icon, String title, String subtitle, String imageAsset) {
+    return Center(
+      child: Container(
+        // ... Container properties ...
+
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Card(
+                  // ... Card properties ...
+
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 8),
+                          child: ListTile(
+                            leading: CircleAvatar(child: Icon(icon)),
+                            title: Text(title,style: TextStyle(fontWeight: FontWeight.bold),),
+                            subtitle: Text(subtitle),
+                            trailing: Icon(Icons.more_horiz),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Container(
+                            child: Image.asset(imageAsset,height: 150,width: 450,),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Fourth extends StatefulWidget {
+  const Fourth({super.key});
+
+  @override
+  State<Fourth> createState() => _FourthState();
+}
+
+class _FourthState extends State<Fourth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(child: Text("Setting",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class Third extends StatelessWidget {
+  const Third({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage('asset/mehraj.jpg'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Text(
+                          "Mehraj Ibne Halim",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Center(
+                          child: Text("Introvert | Photo freak | Wonderlust"),
+                        ),
+                      ),
+                      Center(child: Text("Beautiful moments are to be trusted and")),
+                      Center(child: Text("That's why I capture it")),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          top: 10,
+                          bottom: 5,
+                          right: 10,
+                        ),
+                        child: Card(
+                          color: Colors.indigo,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Hosted",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    Text(
+                                      "87",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Followers",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    Text(
+                                      "2374",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Following",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    Text(
+                                      "2374",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 0),
+                  child: Text(
+                    "Recent",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Card(
+                  child: Expanded(
+                    child: SliderWidget(),
+                  ),
+                ),
+              ),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 0),
+                  child: Text(
+                    "Featured",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('asset/mehraj.jpg'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all( 5),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('asset/music3.jpg'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all( 5),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('asset/mehraj.jpg'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SliderWidget extends StatefulWidget {
+  const SliderWidget({Key? key}) : super(key: key);
+
+  @override
+  _SliderWidgetState createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  final List<AssetImage> images = const [
+    AssetImage("asset/music3.jpg"),
+    AssetImage("asset/music3.jpg"),
+    AssetImage("asset/music3.jpg"),
+    AssetImage("asset/music3.jpg"),
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 80,
+          child: PageView.builder(
+            itemCount: images.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Image(
+                image: images[index],height: 80,width: 80,
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: images.map((image) {
+            int index = images.indexOf(image);
+            return Container(
+              width: 10,
+              height: 10,
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index ? Colors.blue : Colors.grey,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
+
+
+
 
